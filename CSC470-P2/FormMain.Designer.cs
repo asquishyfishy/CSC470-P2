a0199870c2a052435ccf737546149734dcd03496
Tim <timothy.sereda@trojans.dsu.edu>
2021-09-14 20:36:32 -0500

    Added Fish Panel + Button

--- CSC470-P2/FormMain.Designer.cs
+++ CSC470-P2/FormMain.Designer.cs
@@ -29,13 +29,92 @@ namespace CSC470_P2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FishLabel = new System.Windows.Forms.Label();
+            this.crappieButton = new System.Windows.Forms.RadioButton();
+            this.perchButton = new System.Windows.Forms.RadioButton();
+            this.walleyeButton = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.walleyeButton);
+            this.panel1.Controls.Add(this.perchButton);
+            this.panel1.Controls.Add(this.crappieButton);
+            this.panel1.Location = new System.Drawing.Point(170, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(421, 273);
+            this.panel1.TabIndex = 0;
+            // 
+            // FishLabel
+            // 
+            this.FishLabel.AutoSize = true;
+            this.FishLabel.Location = new System.Drawing.Point(195, 31);
+            this.FishLabel.Name = "FishLabel";
+            this.FishLabel.Size = new System.Drawing.Size(103, 13);
+            this.FishLabel.TabIndex = 1;
+            this.FishLabel.Text = "Choose a fish to see";
+            this.FishLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // crappieButton
+            // 
+            this.crappieButton.AutoSize = true;
+            this.crappieButton.Location = new System.Drawing.Point(67, 74);
+            this.crappieButton.Name = "crappieButton";
+            this.crappieButton.Size = new System.Drawing.Size(61, 17);
+            this.crappieButton.TabIndex = 0;
+            this.crappieButton.TabStop = true;
+            this.crappieButton.Text = "Crappie";
+            this.crappieButton.UseVisualStyleBackColor = true;
+            this.crappieButton.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // perchButton
+            // 
+            this.perchButton.AutoSize = true;
+            this.perchButton.Location = new System.Drawing.Point(79, 134);
+            this.perchButton.Name = "perchButton";
+            this.perchButton.Size = new System.Drawing.Size(53, 17);
+            this.perchButton.TabIndex = 1;
+            this.perchButton.TabStop = true;
+            this.perchButton.Text = "Perch";
+            this.perchButton.UseVisualStyleBackColor = true;
+            // 
+            // walleyeButton
+            // 
+            this.walleyeButton.AutoSize = true;
+            this.walleyeButton.Location = new System.Drawing.Point(79, 194);
+            this.walleyeButton.Name = "walleyeButton";
+            this.walleyeButton.Size = new System.Drawing.Size(63, 17);
+            this.walleyeButton.TabIndex = 2;
+            this.walleyeButton.TabStop = true;
+            this.walleyeButton.Text = "Walleye";
+            this.walleyeButton.UseVisualStyleBackColor = true;
+            this.walleyeButton.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FishLabel);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label FishLabel;
+        private System.Windows.Forms.RadioButton walleyeButton;
+        private System.Windows.Forms.RadioButton perchButton;
+        private System.Windows.Forms.RadioButton crappieButton;
     }
 }
 
--- CSC470-P2/FormMain.cs
+++ CSC470-P2/FormMain.cs
@@ -16,5 +16,20 @@ namespace CSC470_P2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
