2c9a35c50fb8f87ca03deb43644bafcdd533ec98
Tim <timothy.sereda@trojans.dsu.edu>
2021-09-14 20:38:32 -0500

    Revert "Added Fish Panel + Button"
    This reverts commit a0199870c2a052435ccf737546149734dcd03496.

--- CSC470-P2/FormMain.cs
+++ CSC470-P2/FormMain.cs
@@ -16,20 +16,5 @@ namespace CSC470_P2
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
